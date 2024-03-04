c0460d1a81a2019018d2fab6c1480479e63f5d25
Ben Spivak <49567266+benjamwd@users.noreply.github.com>
2024-03-04 19:08:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+
 document.addEventListener("DOMContentLoaded", function() {
   const categoryTitles = document.querySelectorAll('.category-title');
-
   categoryTitles.forEach(function(title) {
     title.addEventListener('click', function() {
       const items = this.nextElementSibling;
